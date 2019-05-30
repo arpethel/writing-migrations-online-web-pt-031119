70aac541754cb2e389c502d4aa97c703d0a19f36
Ryan Pethel <arpethel@gmail.com>
2019-05-30 02:54:54 +0000

    Automatically backed up by Learn

---
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -0,0 +1,8 @@
+class ChangeBirthdateDatatype < ActiveRecord::Migration[5.1]
+  def change
+    change_column :students do |t|
+      t.integer :grade
+      t.string :birthdate
+    end
+  end
+end
